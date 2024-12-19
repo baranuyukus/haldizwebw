1fb05558b6145dc1ac1049108e7e5aa35f186ace
baranuyukus <48299018+baranuyukus@users.noreply.github.com>
2024-12-19 14:05:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -846,4 +846,4 @@ def admin_make_admin(user_id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, port=5000) 
+    app.run(host='0.0.0.0', port=5000, debug=True) 
